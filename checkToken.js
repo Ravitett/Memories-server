a7287c6cda0db8fe26c5af00bb1d07a0fef6e93b
RotemEzra1 <rotem19961@gmail.com>
2021-12-14 18:03:52 +0200

    the token complete

--- checkToken.js
+++ checkToken.js
@@ -1,1 +1,20 @@
 const jwt = require("jsonwebtoken");
+const express =require("express");
+
+const checkToken = async(req, res, next) => {
+    let token = req.header("x-API-key");
+    if(!token) {
+        return res.status(401).json({message: "you must send token"});
+    }
+    try {
+        let verifyToken = jwt.verify(token, "ROTEMSECRET");
+        console.log(verifyToken);
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: "error token"});
+
+    }
+}
+
+exports.checkToken = checkToken;
