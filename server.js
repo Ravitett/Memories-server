a1514f0e718ece28c9b3918b981877385aa2f713
RotemEzra1 <rotem19961@gmail.com>
2022-02-16 00:59:32 +0200

    creat logs

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require("cors");
+const winston = require("winston");
+const moment = require("moment");
 
 const app = express();
 
@@ -8,6 +10,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.File({ filename: 'logs.txt' }),
+        new winston.transports.Console()
+    ]
+});
+
+app.all("*", (req, res, next)=> {
+    logger.info(`Got a ${req.method} request - Route ${req.url} - ${moment().format()}`);
+    next();
+})
+
 const {authRouter} = require('./routers/authRouter');
 app.use('/auth' ,authRouter);
 
@@ -15,6 +31,7 @@ const {userRouter} = require('./routers/userRouter');
 app.use('/api/user' ,userRouter);
 
 const {memoryRouter} = require('./routers/memoryRouter');
+const { updateLocale } = require('moment');
 app.use('/api/memories', memoryRouter);
 
 app.all('*',(req,res) => {
