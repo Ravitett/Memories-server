92c96d5901e02711e3e67b9686546bf5cd6dc353
Ravitett <ravitett@gmail.com>
2022-02-16 00:48:09 +0200

    create check token with jsonwebtoken

---
+++ services/checkToken.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const {userModel} = require("../models/userModel");
+
+const checkToken = async(req, res, next) => {
+    let token = req.header("autorisation");
+    if(!token) {
+        return res.status(401).json({message: "you must send token"});
+    }
+    try {
+        let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+        res.locals.userType = await getUserType(verifyToken._id);
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: "error token"});
+    }
+}
+
+exports.checkToken = checkToken;
+
+const getUserType = async(id) => {   
+    try {
+        let data = await userModel.findOne({_id: id});
+        return data.userType;   
+    } catch (error) {
+        console.log(error);
+        return "u";
+    } 
+}
