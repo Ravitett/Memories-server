5a0eb0376fc3db532b96ab94748d607da9d32b59
RotemEzra1 <rotem19961@gmail.com>
2022-02-15 23:36:53 +0200

    update server file

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const {authRouter} = require('./routers/authRouter');
+app.use('/auth' ,authRouter);
+
 const {userRouter} = require('./routers/userRouter');
-app.use('/api/user',userRouter);
+app.use('/api/user' ,userRouter);
 
 const {memoryRouter} = require('./routers/memoryRouter');
-app.use('/api/memories',memoryRouter);
+app.use('/api/memories', memoryRouter);
 
 app.all('*',(req,res) => {
-    res.send("Page not found");
+    res.status(404).json({status:"error", message:`Route not found!`});
 });
 
-app.listen(3006);
+const port = process.env.PORT || 3006;
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
